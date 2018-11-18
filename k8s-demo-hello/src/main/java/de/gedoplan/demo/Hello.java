bcd5107aab1ec0b76d1addd757670e439e363407
Dirk Weil <dirk.weil@gedoplan.de>
2018-11-18 15:58:07 +0100

    Init

---
+++ k8s-demo-hello/src/main/java/de/gedoplan/demo/Hello.java
@@ -0,0 +1,12 @@
+package de.gedoplan.demo;
+
+import java.net.InetAddress;
+
+public class Hello {
+
+  public static void main(String[] args) throws Exception {
+
+    System.out.println("\"Hello World!\" from " + InetAddress.getLocalHost());
+  }
+
+}
